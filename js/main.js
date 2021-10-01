9e0ec5f37312848acbb07023a9b052a22b6aeaf2
davidlp90 <davidivan.eda@gmail.com>
2021-10-01 13:48:17 -0300

    update with loading page
    loading page added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    let contenedor = document.getElementById('contenedor_carga');
+
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+}
